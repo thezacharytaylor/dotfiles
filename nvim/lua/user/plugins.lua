98a40642ae7b84cc5354a0950255a12fc65df287
Zachary Taylor <zacharytaylor@Zacharys-MacBook-Pro.local>
2023-04-21 17:44:24 -0500

    Neovim plugins and themes

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -39,6 +39,105 @@ use({
 use('christoomey/vim-tmux-navigator')
 use('farmergreg/vim-lastplace')
 use('tpope/vim-commentary')
+use('tpope/vim-repeat')
+use('tpope/vim-surround')
+use('tpope/vim-eunuch') -- Adds :Rename, :SudoWrite
+use('tpope/vim-unimpaired') -- Adds [b and other handy mappings
+use('tpope/vim-sleuth') -- Indent autodetection with editorconfig support
+use('jessarcher/vim-heritage') -- Automatically create parent dirs when saving
+use('nelstrom/vim-visual-star-search')
+
+-- use({
+--   'tpope/vim-projectionist',
+--   requires = 'tpope/vim-dispatch',
+--   config = function()
+--     require('user.plugins.projectionist')
+--   end,
+-- })
+
+-- use({
+--   'tommcdo/vim-lion',
+--   config = function()
+--     require('user.plugins.lion')
+--   end,
+-- })
+
+use({
+  'whatyouhide/vim-textobj-xmlattr',
+  requires = 'kana/vim-textobj-user',
+})
+
+-- use({
+--   'sickill/vim-pasta',
+--   config = function()
+--     require('user.plugins.pasta')
+--   end,
+-- })
+
+use({
+  'famiu/bufdelete.nvim',
+  config = function()
+    vim.keymap.set('n', '<Leader>q', ':Bdelete<CR>')
+  end,
+})
+
+-- use({
+--   'lukas-reineke/indent-blankline.nvim',
+--   config = function()
+--     require('user.plugins.indent-blankline')
+--   end,
+-- })
+
+-- use({
+--   'AndrewRadev/splitjoin.vim',
+--   config = function()
+--     require('user.plugins.splitjoin')
+--   end,
+-- })
+
+use({
+  'windwp/nvim-autopairs',
+  config = function()
+    require('nvim-autopairs').setup()
+  end,
+})
+
+-- use({
+--   'akinsho/bufferline.nvim',
+--   requires = 'kyazdani42/nvim-web-devicons',
+--   after = 'gruvbox.nvim',
+--   config = function()
+--     require('user.plugins.bufferline')
+--   end,
+-- })
+
+-- use({
+--   'nvim-lualine/lualine.nvim',
+--   requires = 'kyazdani42/nvim-web-devicons',
+--   config = function()
+--     require('user.plugins.lualine')
+--   end,
+-- })
+
+-- use({
+--   'kyazdani42/nvim-tree.lua',
+--   requires = 'kyazdani42/nvim-web-devicons',
+--   config = function()
+--     require('user.plugins.nvim-tree')
+--   end,
+-- })
+
+-- use({
+--   'karb94/neoscroll.nvim',
+--   config = function()
+--     require('user.plugins.neoscroll')
+--   end,
+-- })
+
+-- Copilot
+use('github/copilot.vim')
+
+-- Themes
 use('sainnhe/gruvbox-material')
 use {'ellisonleao/gruvbox.nvim'}
 -- use({

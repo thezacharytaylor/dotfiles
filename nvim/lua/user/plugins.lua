e570315eba38f727c7dc2b5c89ceba2d895a90e1
Zachary Taylor <zacharytaylor@Zacharys-MacBook-Pro.local>
2023-04-21 19:20:53 -0500

    More plugins

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -47,32 +47,32 @@ use('tpope/vim-sleuth') -- Indent autodetection with editorconfig support
 use('jessarcher/vim-heritage') -- Automatically create parent dirs when saving
 use('nelstrom/vim-visual-star-search')
 
--- use({
---   'tpope/vim-projectionist',
---   requires = 'tpope/vim-dispatch',
---   config = function()
---     require('user.plugins.projectionist')
---   end,
--- })
+use({ 'tpope/vim-projectionist',
+  requires = 'tpope/vim-dispatch',
+  config = function()
+    require('user.plugins.projectionist')
+  end,
+})
 
--- use({
---   'tommcdo/vim-lion',
---   config = function()
---     require('user.plugins.lion')
---   end,
--- })
+use({
+  'tommcdo/vim-lion',
+  config = function()
+    require('user.plugins.lion')
+  end,
+})
 
 use({
   'whatyouhide/vim-textobj-xmlattr',
   requires = 'kana/vim-textobj-user',
 })
 
--- use({
---   'sickill/vim-pasta',
---   config = function()
---     require('user.plugins.pasta')
---   end,
--- })
+use({
+  'sickill/vim-pasta',
+
+  config = function()
+    require('user.plugins.pasta')
+  end,
+})
 
 use({
   'famiu/bufdelete.nvim',
@@ -81,19 +81,19 @@ use({
   end,
 })
 
--- use({
---   'lukas-reineke/indent-blankline.nvim',
---   config = function()
---     require('user.plugins.indent-blankline')
---   end,
--- })
+use({
+  'lukas-reineke/indent-blankline.nvim',
+  config = function()
+    require('user.plugins.indent-blankline')
+  end,
+})
 
--- use({
---   'AndrewRadev/splitjoin.vim',
---   config = function()
---     require('user.plugins.splitjoin')
---   end,
--- })
+use({
+  'AndrewRadev/splitjoin.vim',
+  config = function()
+    require('user.plugins.splitjoin')
+  end,
+})
 
 use({
   'windwp/nvim-autopairs',
@@ -102,37 +102,219 @@ use({
   end,
 })
 
+use({
+  'akinsho/bufferline.nvim',
+  requires = 'kyazdani42/nvim-web-devicons',
+  after = 'gruvbox.nvim',
+  config = function()
+    require('user.plugins.bufferline')
+  end,
+})
+
+use({
+  'nvim-lualine/lualine.nvim',
+  requires = 'kyazdani42/nvim-web-devicons',
+  config = function()
+    require('user.plugins.lualine')
+  end,
+})
+
+use({
+  'kyazdani42/nvim-tree.lua',
+  requires = 'kyazdani42/nvim-web-devicons',
+  config = function()
+    require('user.plugins.nvim-tree')
+  end,
+})
+
+use({
+  'karb94/neoscroll.nvim',
+  config = function()
+    require('user.plugins.neoscroll')
+  end,
+})
+
+use({
+  'vim-test/vim-test',
+  config = function()
+    require('user.plugins.vim-test')
+  end,
+})
+
+use({
+  'voldikss/vim-floaterm',
+  config = function()
+    require('user.plugins.floaterm')
+  end,
+})
+
+use({
+  'nvim-telescope/telescope.nvim',
+  requires = {
+    { 'nvim-lua/plenary.nvim' },
+    { 'kyazdani42/nvim-web-devicons' },
+    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+    { 'nvim-telescope/telescope-live-grep-args.nvim' },
+  },
+  config = function()
+    require('user.plugins.telescope')
+  end,
+})
+
+use({
+  'nvim-treesitter/nvim-treesitter',
+  run = ':TSUpdate',
+  requires = {
+    'nvim-treesitter/playground',
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    'JoosepAlviste/nvim-ts-context-commentstring',
+  },
+  config = function()
+    require('user.plugins.treesitter')
+  end,
+})
+
+use({
+  'tpope/vim-fugitive',
+  requires = 'tpope/vim-rhubarb',
+  cmd = 'G',
+})
+
+use({
+  'lewis6991/gitsigns.nvim',
+  requires = 'nvim-lua/plenary.nvim',
+  config = function()
+    require('gitsigns').setup({
+      sign_priority = 20,
+      on_attach = function(bufnr)
+        vim.keymap.set('n', ']h', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true, buffer = bufnr })
+        vim.keymap.set('n', '[h', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true, buffer = bufnr })
+      end,
+    })
+  end,
+})
+
 -- use({
---   'akinsho/bufferline.nvim',
---   requires = 'kyazdani42/nvim-web-devicons',
---   after = 'gruvbox.nvim',
+--   'neovim/nvim-lspconfig',
+--   requires = {
+--     'b0o/schemastore.nvim',
+--     'folke/lsp-colors.nvim',
+--   },
 --   config = function()
---     require('user.plugins.bufferline')
+--     require('user.plugins.lspconfig')
 --   end,
 -- })
 
--- use({
---   'nvim-lualine/lualine.nvim',
---   requires = 'kyazdani42/nvim-web-devicons',
---   config = function()
---     require('user.plugins.lualine')
---   end,
--- })
+use({
+  'weilbith/nvim-code-action-menu',
+  cmd = 'CodeActionMenu',
+})
 
--- use({
---   'kyazdani42/nvim-tree.lua',
---   requires = 'kyazdani42/nvim-web-devicons',
---   config = function()
---     require('user.plugins.nvim-tree')
---   end,
--- })
+use({
+  'jose-elias-alvarez/null-ls.nvim',
+  config = function()
+    require('user.plugins.null-ls')
+  end,
+})
 
--- use({
---   'karb94/neoscroll.nvim',
+-- use {
+--   'j-hui/fidget.nvim',
 --   config = function()
---     require('user.plugins.neoscroll')
+--     require('fidget').setup{
+--       align = {
+--         bottom = false
+--       }
+--     }
 --   end,
--- })
+-- }
+
+use({
+  'L3MON4D3/LuaSnip',
+  config = function()
+    require('user.plugins.luasnip')
+  end,
+})
+
+use({
+  'hrsh7th/nvim-cmp',
+  requires = {
+    'L3MON4D3/LuaSnip',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-nvim-lsp-signature-help',
+    'hrsh7th/cmp-nvim-lua',
+    'jessarcher/cmp-path',
+    'onsails/lspkind-nvim',
+    'saadparwaiz1/cmp_luasnip',
+  },
+  config = function()
+    require('user.plugins.cmp')
+  end,
+})
+
+use({
+  'phpactor/phpactor',
+  branch = 'master',
+  ft = 'php',
+  run = 'composer install --no-dev -o',
+  config = function()
+    require('user.plugins.phpactor')
+  end,
+})
+
+-- Experimental
+
+use({
+  'luukvbaal/stabilize.nvim',
+  config = function()
+    require('stabilize').setup()
+  end,
+})
+
+use({
+  'glepnir/dashboard-nvim',
+  config = function()
+    require('user.plugins.dashboard')
+  end,
+})
+
+use({
+  'danymat/neogen',
+  config = function()
+    require('neogen').setup({})
+  end,
+  requires = 'nvim-treesitter/nvim-treesitter',
+})
+
+use({
+  'sheerun/vim-polyglot',
+})
+
+-- Rename in a popup window
+use({
+  'hood/popui.nvim',
+  requires = 'RishabhRD/popfix',
+  config = function()
+    vim.ui.select = require('popui.ui-overrider')
+    vim.ui.input = require('popui.input-overrider')
+  end,
+})
+
+use({
+  'folke/trouble.nvim',
+  requires = 'kyazdani42/nvim-web-devicons',
+  config = function()
+    require('trouble').setup()
+  end,
+})
+
+use({
+  'antoinemadec/FixCursorHold.nvim',
+  config = function()
+    vim.g.cursorhold_updatetime = 100
+  end,
+})
 
 -- Copilot
 use('github/copilot.vim')

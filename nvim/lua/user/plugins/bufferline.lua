4eca223baebf3178b3786a4006307e3d6c2bbc65
Zachary Taylor <zacharytaylor@Zacharys-MacBook-Pro.local>
2023-04-22 10:42:41 -0500

    Buffer line style tweaks

--- nvim/lua/user/plugins/bufferline.lua
+++ nvim/lua/user/plugins/bufferline.lua
@@ -19,61 +19,61 @@ require('bufferline').setup({
     custom_areas = {
       left = function()
         return {
-          { text = '    ', fg = '#8fff6d' },
+          { text = '    ', fg = '#fb4934' }, --󰆧
         }
       end,
     },
   },
   highlights = {
-    fill = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    background = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    tab = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    tab_close = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    close_button = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-      fg = { attribute = 'fg', highlight = 'StatusLineNonText' },
-    },
-    close_button_visible = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-      fg = { attribute = 'fg', highlight = 'StatusLineNonText' },
-    },
-    close_button_selected = {
-      fg = { attribute = 'fg', highlight = 'StatusLineNonText' },
-    },
-    buffer_visible = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    modified = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    modified_visible = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    duplicate = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    duplicate_visible = {
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    separator = {
-      fg = { attribute = 'bg', highlight = 'StatusLine' },
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
-    separator_selected = {
-      fg = { attribute = 'bg', highlight = 'StatusLine' },
-      bg = { attribute = 'bg', highlight = 'Normal' }
-    },
-    separator_visible = {
-      fg = { attribute = 'bg', highlight = 'StatusLine' },
-      bg = { attribute = 'bg', highlight = 'StatusLine' },
-    },
+    -- fill = {
+    --   bg = { attribute = 'bg', highlight = 'StatusLine' },
+    -- },
+    -- background = {
+    --   bg = { attribute = 'bg', highlight = 'StatusLine' },
+    -- },
+    -- tab = {
+    --   bg = { attribute = 'bg', highlight = 'StatusLine' },
+    -- },
+    -- tab_close = {
+    --   bg = { attribiute = 'bg', highlight = 'StatusLine' },
+    -- },
+    -- close_button = {
+    --   bg = { attribute = 'bg', highlight = 'StatusLine' },
+    --   fg = { attribute = 'fg', highlight = 'StatusLineNonText' },
+    -- },
+    -- close_button_visible = {
+    --   bg = { attribute = 'bg', highlight = 'StatusLine' },
+    --   fg = { attribute = 'fg', highlight = 'StatusLineNonText' },
+    -- },
+    -- close_button_selected = {
+    --   fg = { attribute = 'fg', highlight = 'StatusLineNonText' },
+    -- },
+    -- buffer_visible = {
+    --   bg = { attribute = 'bg', highlight = 'StatusLine' },
+    -- },
+    -- modified = {
+    --   bg = { attribute = 'bg', highlight = 'StatusLine' },
+    -- },
+    -- modified_visible = {
+    --   bg = { attribute = 'bg', highlight = 'StatusLine' },
+    -- },
+    -- duplicate = {
+    --   bg = { attribute = 'bg', highlight = 'StatusLine' },
+    -- },
+    -- duplicate_visible = {
+    --   bg = { attribute = 'bg', highlight = 'StatusLine' },
+    -- },
+    -- separator = {
+    --   fg = { attribute = 'bg', highlight = 'StatusLine' },
+    --   bg = { attribute = 'bg', highlight = 'StatusLine' },
+    -- },
+    -- separator_selected = {
+    --   fg = { attribute = 'bg', highlight = 'StatusLine' },
+    --   bg = { attribute = 'bg', highlight = 'Normal' }
+    -- },
+    -- separator_visible = {
+    --   fg = { attribute = 'bg', highlight = 'StatusLine' },
+    --   bg = { attribute = 'bg', highlight = 'StatusLine' },
+    -- },
   },
 })

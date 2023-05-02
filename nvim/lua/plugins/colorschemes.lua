32139a693e94918db4886b789e069e2af0012691
Zachary Taylor <zacharytaylor@Zacharys-MacBook-Pro.local>
2023-05-02 16:39:58 -0500

    New optional color scheme

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -2,11 +2,25 @@ return {
   {
     -- 'ellisonleao/gruvbox.nvim',
     "eddyekofo94/gruvbox-flat.nvim",
+    config = function()
+      vim.g.gruvbox_flat_style = "dark"
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      theme = "dragon",
+      background = { -- map the value of 'background' option to a theme
+        dark = "dragon", -- try "dragon" because "wave" is default !
+        light = "lotus",
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox-flat",
+      -- colorscheme = "kanagawa",
     },
   },
 }

c544b2c0685551c4516e47fa703c920ed7f19cc8
Zachary Taylor <zacharytaylor@Zacharys-MacBook-Pro.local>
2023-05-04 08:56:59 -0500

    Updating mason and null-ls for phpcs and blade

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -4,6 +4,7 @@ return {
     opts = function(_, opts)
       table.insert(opts.ensure_installed, "prettierd")
       table.insert(opts.ensure_installed, "php-cs-fixer")
+      table.insert(opts.ensure_installed, "blade-formatter")
     end,
   },
 }
--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -4,8 +4,9 @@ return {
     opts = function(_, opts)
       local nls = require("null-ls")
       table.insert(opts.sources, nls.builtins.formatting.prettierd)
-      table.insert(opts.sources, nls.builtins.diagnostics.phpcsfixer)
+      table.insert(opts.sources, nls.builtins.diagnostics.phpcs)
       table.insert(opts.sources, nls.builtins.formatting.phpcsfixer)
+      table.insert(opts.sources, nls.builtins.formatting.bladeformatter)
     end,
   },
 }

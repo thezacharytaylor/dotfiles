27ea037e0a0ae8672d05b5dacf00a2012f8fac22
Zachary Taylor <zacharytaylor@tutamail.com>
2023-05-17 10:31:56 -0500

    up script tweaks

--- scripts/up.sh
+++ scripts/up.sh
@@ -3,54 +3,52 @@
 # If using zsh, then add this function to ~/.zshrc:
 # up() { . up.sh "$1"; }
 
-[[ -z "$1" ]] && { echo "usage: up [site]"; return; }
+[[ -z "$1" ]] && {
+	echo "usage: up [site]"
+	return
+}
 
 KINDIR="$HOME/Kinetic/$1"
 echo "$KINDIR"
 
 # Check for valid working directory or symbolic link
-if [ $1 = "." ]
-then
-    echo "🤘 Current dir"
-    WORKDIR="."
-    TRELLIS=false
-elif [[ -d "$KINDIR" ]]
-then
-    echo "🤘 Let's do this"
-    WORKDIR=$KINDIR
+if [ $1 = "." ]; then
+	echo "🤘 Current dir"
+	WORKDIR="."
+	TRELLIS=false
+elif [[ -d "$KINDIR" ]]; then
+	echo "🤘 Let's do this"
+	WORKDIR=$KINDIR
 else
-    echo "⚠️  Directory doesn't exist."
-    return
+	echo "⚠️  Directory doesn't exist."
+	return
 fi
 
 # Change to the working directory and do work
-cd $WORKDIR;
+cd $WORKDIR
 echo "✅ Open VS Code"
 code .
 
 # If .valetrc, then switch PHP version
 # To use, add a .valetrc file to the folder root with the following: php=php@8.1
-if [[ -f "$WORKDIR/.valetrc" ]]
-then
-    echo "✅ Switching PHP version"
-    valet use
+if [[ -f "$WORKDIR/.valetrc" ]]; then
+	echo "✅ Switching PHP version"
+	valet use
 fi
 
 # If .git, then do git stuff
-if [[ -d "$WORKDIR/.git" ]]
-then
-    echo "✅ Fetching repo"
-    git pull
+if [[ -d "$WORKDIR/.git" ]]; then
+	echo "✅ Fetching repo"
+	git pull
 
-    echo "✅ Open Lazygit"
-    lg .
+	echo "✅ Open Lazygit"
+	lg
 fi
 
 # If trellis, then init it.
-if [[ -d "$WORKDIR/trellis" ]]
-then
-    echo "✅ Trellis"
-    trellis init
+if [[ -d "$WORKDIR/trellis" ]]; then
+	echo "✅ Trellis"
+	trellis init
 fi
 
 echo "👯‍♀️"

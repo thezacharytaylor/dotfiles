2d9169b6e53c697bdd1df7ac68fa3bfc61bd327d
Zachary Taylor <zacharytaylor@tutamail.com>
2023-05-19 16:19:56 -0500

    lvim config tweak, key map and theme

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -2,6 +2,7 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
 -- Theme
 lvim.plugins = {{
     "neanias/everforest-nvim",
@@ -30,4 +31,8 @@ lvim.plugins = {{
 }}
 
 -- Settings
-lvim.builtin.lualine.options.theme = 'everforest'
+lvim.colorscheme = "everforest"
+-- lvim.builtin.lualine.options.theme = "everforest"
+
+-- Mappings
+lvim.keys.insert_mode["jk"] = "<ESC>"

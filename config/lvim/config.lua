0b50cb9785532a9f7c1a999fb8f7faf74b297daa
Zachary Taylor <zacharytaylor@tutamail.com>
2023-05-20 11:06:33 -0500

    a few qol tweaks for lvim

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -34,5 +34,103 @@ lvim.plugins = {{
 lvim.colorscheme = "everforest"
 -- lvim.builtin.lualine.options.theme = "everforest"
 
+-- Plugins
+table.insert(lvim.plugins, {
+  "zbirenbaum/copilot-cmp",
+  event = "InsertEnter",
+  dependencies = { "zbirenbaum/copilot.lua" },
+  config = function()
+    vim.defer_fn(function()
+      require("copilot").setup() -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+      require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+    end, 100)
+  end,
+})
+
+table.insert(lvim.plugins, {
+  "tpope/vim-surround",
+
+  -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+  -- setup = function()
+    --  vim.o.timeoutlen = 500
+  -- end
+})
+
+table.insert(lvim.plugins, { "tpope/vim-repeat" })
+
+table.insert(lvim.plugins, {
+  "felipec/vim-sanegx",
+  event = "BufRead",
+})
+
+table.insert(lvim.plugins, {
+  "karb94/neoscroll.nvim",
+  event = "WinScrolled",
+  config = function()
+  require('neoscroll').setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+        '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+        hide_cursor = true,          -- Hide cursor while scrolling
+        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+        respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil,        -- Default easing function
+        pre_hook = nil,              -- Function to run before the scrolling animation starts
+        post_hook = nil,              -- Function to run after the scrolling animation ends
+        })
+  end
+})
+
+table.insert(lvim.plugins,  {
+  "ethanholz/nvim-lastplace",
+  event = "BufRead",
+  config = function()
+   require("nvim-lastplace").setup({
+    lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+    lastplace_ignore_filetype = {
+     "gitcommit", "gitrebase", "svn", "hgcommit",
+    },
+    lastplace_open_folds = true,
+   })
+  end,
+ })
+
+table.insert(lvim.plugins, {
+  "windwp/nvim-ts-autotag",
+  config = function()
+    require("nvim-ts-autotag").setup()
+  end,
+})
+
+table.insert(lvim.plugins, {
+  "mrjones2014/nvim-ts-rainbow",
+})
+
+lvim.builtin.treesitter.rainbow.enable = true
+
+table.insert(lvim.plugins, {
+    "romgrk/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup{
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        throttle = true, -- Throttles plugin updates (may improve performance)
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+          -- For all filetypes
+          -- Note that setting an entry here replaces all other patterns for this entry.
+          -- By setting the 'default' entry below, you can control which nodes you want to
+          -- appear in the context window.
+          default = {
+            'class',
+            'function',
+            'method',
+          },
+        },
+      }
+    end
+  })
+
 -- Mappings
 lvim.keys.insert_mode["jk"] = "<ESC>"

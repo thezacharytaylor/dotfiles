d844c5ef7123f2a307eaf466d68ecfa5e010874e
Zachary Taylor <zacharytaylor@tutamail.com>
2023-07-11 10:39:28 -0500

    formatting lvim config

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -7,31 +7,31 @@
 -- Theme
 ------------------------
 lvim.plugins = {
-	{
-		"neanias/everforest-nvim",
-		version = false,
-		lazy = false,
-		priority = 1000, -- make sure to load this before all the other start plugins
-		-- Optional; default configuration will be used if setup isn't called.
-		config = function()
-			require("everforest").setup({
-				-- Controls the "hardness" of the background. Options are "soft", "medium" or "hard".
-				-- Default is "medium".
-				background = "dark",
-				-- How much of the background should be transparent. Options are 0, 1 or 2.
-				-- Default is 0.
-				--
-				-- 2 will have more UI components be transparent (e.g. status line
-				-- background).
-				transparent_background_level = 0,
-				-- Whether italics should be used for keywords, builtin types and more.
-				italics = false,
-				-- Disable italic fonts for comments. Comments are in italics by default, set
-				-- this to `true` to make them _not_ italic!
-				disable_italic_comments = false,
-			})
-		end,
-	},
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require("everforest").setup({
+        -- Controls the "hardness" of the background. Options are "soft", "medium" or "hard".
+        -- Default is "medium".
+        background = "dark",
+        -- How much of the background should be transparent. Options are 0, 1 or 2.
+        -- Default is 0.
+        --
+        -- 2 will have more UI components be transparent (e.g. status line
+        -- background).
+        transparent_background_level = 0,
+        -- Whether italics should be used for keywords, builtin types and more.
+        italics = false,
+        -- Disable italic fonts for comments. Comments are in italics by default, set
+        -- this to `true` to make them _not_ italic!
+        disable_italic_comments = false,
+      })
+    end,
+  },
 }
 
 ------------------------
@@ -47,13 +47,13 @@ vim.opt.fillchars = { eob = " " }
 -- Treesitter
 ------------------------
 lvim.builtin.treesitter.ensure_installed = {
-	"javascript",
-	"json",
-	"lua",
-	"typescript",
-	"tsx",
-	"css",
-	"php",
+  "javascript",
+  "json",
+  "lua",
+  "typescript",
+  "tsx",
+  "css",
+  "php",
 }
 
 -- lvim.lsp.installer.setup.ensure_installed = {
@@ -69,111 +69,111 @@ lvim.builtin.treesitter.ensure_installed = {
 -- Plugins
 ------------------------
 table.insert(lvim.plugins, {
-	"zbirenbaum/copilot-cmp",
-	event = "InsertEnter",
-	dependencies = { "zbirenbaum/copilot.lua" },
-	config = function()
-		vim.defer_fn(function()
-			require("copilot").setup() -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
-			require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
-		end, 100)
-	end,
+  "zbirenbaum/copilot-cmp",
+  event = "InsertEnter",
+  dependencies = { "zbirenbaum/copilot.lua" },
+  config = function()
+    vim.defer_fn(function()
+      require("copilot").setup() -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+      require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+    end, 100)
+  end,
 })
 
 table.insert(lvim.plugins, {
-	"tpope/vim-surround",
+  "tpope/vim-surround",
 
-	-- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
-	-- setup = function()
-	--  vim.o.timeoutlen = 500
-	-- end
+  -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+  -- setup = function()
+  --  vim.o.timeoutlen = 500
+  -- end
 })
 
 table.insert(lvim.plugins, { "tpope/vim-repeat" })
 
 table.insert(lvim.plugins, {
-	"felipec/vim-sanegx",
-	event = "BufRead",
+  "felipec/vim-sanegx",
+  event = "BufRead",
 })
 
 table.insert(lvim.plugins, {
-	"karb94/neoscroll.nvim",
-	event = "WinScrolled",
-	config = function()
-		require("neoscroll").setup({
-			-- All these keys will be mapped to their corresponding default scrolling animation
-			mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
-			hide_cursor = true, -- Hide cursor while scrolling
-			stop_eof = true, -- Stop at <EOF> when scrolling downwards
-			use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-			respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-			cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-			easing_function = nil, -- Default easing function
-			pre_hook = nil, -- Function to run before the scrolling animation starts
-			post_hook = nil, -- Function to run after the scrolling animation ends
-		})
-	end,
+  "karb94/neoscroll.nvim",
+  event = "WinScrolled",
+  config = function()
+    require("neoscroll").setup({
+      -- All these keys will be mapped to their corresponding default scrolling animation
+      mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+      hide_cursor = true, -- Hide cursor while scrolling
+      stop_eof = true, -- Stop at <EOF> when scrolling downwards
+      use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+      respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+      cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+      easing_function = nil, -- Default easing function
+      pre_hook = nil, -- Function to run before the scrolling animation starts
+      post_hook = nil, -- Function to run after the scrolling animation ends
+    })
+  end,
 })
 
 table.insert(lvim.plugins, {
-	"ethanholz/nvim-lastplace",
-	event = "BufRead",
-	config = function()
-		require("nvim-lastplace").setup({
-			lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
-			lastplace_ignore_filetype = {
-				"gitcommit",
-				"gitrebase",
-				"svn",
-				"hgcommit",
-			},
-			lastplace_open_folds = true,
-		})
-	end,
+  "ethanholz/nvim-lastplace",
+  event = "BufRead",
+  config = function()
+    require("nvim-lastplace").setup({
+      lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+      lastplace_ignore_filetype = {
+        "gitcommit",
+        "gitrebase",
+        "svn",
+        "hgcommit",
+      },
+      lastplace_open_folds = true,
+    })
+  end,
 })
 
 table.insert(lvim.plugins, {
-	"windwp/nvim-ts-autotag",
-	config = function()
-		require("nvim-ts-autotag").setup()
-	end,
+  "windwp/nvim-ts-autotag",
+  config = function()
+    require("nvim-ts-autotag").setup()
+  end,
 })
 
 table.insert(lvim.plugins, {
-	"mrjones2014/nvim-ts-rainbow",
+  "mrjones2014/nvim-ts-rainbow",
 })
 
 lvim.builtin.treesitter.rainbow.enable = true
 
 table.insert(lvim.plugins, {
-	"romgrk/nvim-treesitter-context",
-	config = function()
-		require("treesitter-context").setup({
-			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-			throttle = true, -- Throttles plugin updates (may improve performance)
-			max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-			patterns = {
-				-- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-				-- For all filetypes
-				-- Note that setting an entry here replaces all other patterns for this entry.
-				-- By setting the 'default' entry below, you can control which nodes you want to
-				-- appear in the context window.
-				default = {
-					"class",
-					"function",
-					"method",
-				},
-			},
-		})
-	end,
+  "romgrk/nvim-treesitter-context",
+  config = function()
+    require("treesitter-context").setup({
+      enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+      throttle = true, -- Throttles plugin updates (may improve performance)
+      max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+      patterns = {
+        -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+        -- For all filetypes
+        -- Note that setting an entry here replaces all other patterns for this entry.
+        -- By setting the 'default' entry below, you can control which nodes you want to
+        -- appear in the context window.
+        default = {
+          "class",
+          "function",
+          "method",
+        },
+      },
+    })
+  end,
 })
 
 table.insert(lvim.plugins, {
-	"ahmedkhalf/lsp-rooter.nvim",
-	event = "BufRead",
-	config = function()
-		require("lsp-rooter").setup()
-	end,
+  "ahmedkhalf/lsp-rooter.nvim",
+  event = "BufRead",
+  config = function()
+    require("lsp-rooter").setup()
+  end,
 })
 
 ------------------------
@@ -181,41 +181,42 @@ table.insert(lvim.plugins, {
 ------------------------
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
-	{ command = "phpcsfixer", filetypes = { "php" } },
-	{ command = "stylua", filetypes = { "lua" } },
-	{
-		command = "prettierd",
-		extra_args = { "--print-width", "100" },
-		filetypes = {
-			"javascript",
-			"javascriptreact",
-			"typescript",
-			"typescriptreact",
-			"vue",
-			"css",
-			"scss",
-			"less",
-			"html",
-			"json",
-			"jsonc",
-			"yaml",
-			"markdown",
-			"markdown.mdx",
-			"graphql",
-			"handlebars",
-		},
-	},
+  { command = "phpcsfixer", filetypes = { "php" } },
+  { command = "stylua", filetypes = { "lua" } },
+  {
+    command = "prettierd",
+    extra_args = { "--print-width", "100" },
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "vue",
+      "css",
+      "scss",
+      "less",
+      "html",
+      "json",
+      "jsonc",
+      "yaml",
+      "markdown",
+      "markdown.mdx",
+      "graphql",
+      "handlebars",
+      "liquid",
+    },
+  },
 })
 
 lvim.format_on_save.enabled = true
 lvim.format_on_save.pattern = {
-	"*.css",
-	"*.scss",
-	"*.html",
-	"*.json",
-	"*.js",
-	"*.lua",
-	"*.php",
+  "*.css",
+  "*.scss",
+  "*.html",
+  "*.json",
+  "*.js",
+  "*.lua",
+  "*.php",
 }
 
 ------------------------

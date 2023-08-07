5fad38deced33e257cee02fc7cf1c632495bafe7
Zachary Taylor <zacharytaylor@tutamail.com>
2023-08-07 15:40:02 -0500

    cleaning up old files

--- store/nvim.bak/lua/ztaylor/plugins/neoscroll.lua
+++
@@ -1,4 +0,0 @@
-return {require('neoscroll.config').set_mappings({
-    ['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '50'}},
-    ['<C-d>'] = {'scroll', {'vim.wo.scroll', 'true', '50'}}
-})}
--- store/nvimlzy.bak/lua/plugins/lion.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "tommcdo/vim-lion",
-    config = function()
-      vim.g.lion_squeeze_spaces = 1 -- Remove as many spaces as possible when aligning
-    end,
-  },
-}

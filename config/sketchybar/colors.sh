c6f1a22c969c238e891d2cb51aeaa33760a77af1
Zachary Taylor <zacharytaylor@tutamail.com>
2023-08-16 13:17:40 -0500

    sketchybar to catuppuccin

--- config/sketchybar/colors.sh
+++ config/sketchybar/colors.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # Color Palette
-export BLACK=0xff1e2326
-export WHITE=0xffd3c6aa
-export RED=0xffe67e80
-export GREEN=0xffa7c080
-export BLUE=0xff7fbbb3
-export YELLOW=0xffdbbc7f
-export ORANGE=0xffe69875
-export MAGENTA=0xffd699b6
-export GREY=0xff7a8478
+export BLACK=0xff181926
+export WHITE=0xffcad3f5
+export RED=0xffed8796
+export GREEN=0xffa6da95
+export BLUE=0xff8aadf4
+export YELLOW=0xffeed49f
+export ORANGE=0xfff5a97f
+export MAGENTA=0xffc6a0f6
+export GREY=0xff939ab7
 export TRANSPARENT=0x00000000
 
 # General bar colors
-export BAR_COLOR=0xff2e383c
-export BAR_BORDER_COLOR=0xff83c092 # 3c4841
+export BAR_COLOR=0xff1e1e2e
+export BAR_BORDER_COLOR=0xff494d64 #0xa024273a
 export ICON_COLOR=$WHITE # Color of all icons
 export LABEL_COLOR=$WHITE # Color of all labels
-export BACKGROUND_1=0x60374145
-export BACKGROUND_2=0x604f5b58
+export BACKGROUND_1=0x603c3e4f
+export BACKGROUND_2=0x60494d64
 
-export POPUP_BACKGROUND_COLOR=0xff2e383c
+export POPUP_BACKGROUND_COLOR=0xff1e1e2e
 export POPUP_BORDER_COLOR=$WHITE
 
 export SHADOW_COLOR=$BLACK

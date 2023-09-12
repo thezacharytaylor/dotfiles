b8268f3a9f5e3f50429b1b9d47f2d7b8b2bd7eb6
Zachary Taylor <zacharytaylor@tutamail.com>
2023-09-12 10:15:37 -0500

    switching to dark rose pine for home, sbar color organization

--- config/sketchybar/colors.sh
+++ config/sketchybar/colors.sh
@@ -1,28 +1,76 @@
 #!/bin/bash
 
-# Rose Pine Dawn | Catuppuccin
+# Rose Pine Moon | Rose Pine Dawn | Catuppuccin
 
 # Color Palette
-export BLACK=0xfff4ede8 # 0xff181926
-export WHITE=0xff575279 # 0xffcad3f5
-export RED=0xffb4637a # 0xffed8796
-export GREEN=0xff56949f # 0xffa6da95
-export BLUE=0xff286983 # 0xff8aadf4
-export YELLOW=0xffea9d34 # 0xffeed49f
-export ORANGE=0xffd7827e # 0xfff5a97f
-export MAGENTA=0xff907aa9 # 0xffc6a0f6
-export GREY=0xff9893a5 # 0xff939ab7
+# Rose Pine Moon
+# ===============================
+export BLACK=0xff2a283e
+export WHITE=0xffe0def4 # 0xff575279 # 0xffcad3f5
+export RED=0xffeb6f92 # 0xffb4637a # 0xffed8796
+export GREEN=0xff9ccfd8 # 0xff56949f # 0xffa6da95
+export BLUE=0xff3e8fb0 # 0xff286983 # 0xff8aadf4
+export YELLOW=0xfff6c177 # 0xffea9d34 # 0xffeed49f
+export ORANGE=0xffea9a97 # 0xffd7827e # 0xfff5a97f
+export MAGENTA=0xffc4a7e7 # 0xff907aa9 # 0xffc6a0f6
+export GREY=0xff636a86 # 0xff9893a5 # 0xff939ab7
+
+# Rose Pine Dawn
+# ===============================
+# export BLACK=0xfff4ede8
+# export WHITE=0xff575279
+# export RED=0xffb4637a
+# export GREEN=0xff56949f
+# export BLUE=0xff286983
+# export YELLOW=0xffea9d34
+# export ORANGE=0xffd7827e
+# export MAGENTA=0xff907aa9
+# export GREY=0xff9893a5
+
+# Catuppuccin
+# ===============================
+# export BLACK=0xff181926
+# export WHITE=0xffcad3f5
+# export RED=0xffed8796
+# export GREEN=0xffa6da95
+# export BLUE=0xff8aadf4
+# export YELLOW=0xffeed49f
+# export ORANGE=0xfff5a97f
+# export MAGENTA=0xffc6a0f6
+# export GREY=0xff939ab7
+
+# General Palette Colors
+# ===============================
 export TRANSPARENT=0x00000000
 
-# General bar colors
-export BAR_COLOR=0xfffaf4ed # 0xff1e1e2e
-export BAR_BORDER_COLOR=0xffd7827e # 0xff494d64 #0xa024273a
+# Bar Palette Colors
+# Rose Pine Moon
+# ===============================
+export BAR_COLOR=0xff232136 # 0xfffaf4ed # 0xff1e1e2e
+export BAR_BORDER_COLOR=0xffea9a97 # 0xffd7827e # 0xff494d64 #0xa024273a
+export BACKGROUND_1=0xff2a273 # 0xfffffaf3 # 0x603c3e4f
+export BACKGROUND_2=0xff2a283e # 0xfff4ede8 # 0x60494d64
+export POPUP_BACKGROUND_COLOR=0xff393552 # 0xfff2e9e1 # 0xff1e1e2e
+
+# Rose Pine Dawn
+# ===============================
+# export BAR_COLOR=0xfffaf4ed # 0xff1e1e2e
+# export BAR_BORDER_COLOR=0xffd7827e # 0xff494d64 #0xa024273a
+# export BACKGROUND_1=0xfffffaf3 # 0x603c3e4f
+# export BACKGROUND_2=0xfff4ede8 # 0x60494d64
+# export POPUP_BACKGROUND_COLOR=0xfff2e9e1 # 0xff1e1e2e
+
+# Catuppuccin
+# ===============================
+# export BAR_COLOR=0xff1e1e2e
+# export BAR_BORDER_COLOR=0xff494d64 #0xa024273a
+# export BACKGROUND_1=0x603c3e4f
+# export BACKGROUND_2=0x60494d64
+# export POPUP_BACKGROUND_COLOR=0xff1e1e2e
+
+# General Bar Colors
+# ===============================
 export ICON_COLOR=$WHITE # Color of all icons
 export LABEL_COLOR=$WHITE # Color of all labels
-export BACKGROUND_1=0xfffffaf3 # 0x603c3e4f
-export BACKGROUND_2=0xfff4ede8 # 0x60494d64
-
-export POPUP_BACKGROUND_COLOR=0xfff2e9e1 # 0xff1e1e2e
 export POPUP_BORDER_COLOR=$WHITE
-
 export SHADOW_COLOR=$BLACK

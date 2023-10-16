1d451d9edc3bc1f37874f37e396d4c61eae2c21c
Zachary Taylor <zacharytaylor@tutamail.com>
2023-10-16 11:11:07 -0500

    adding hop plugin for nvim

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -154,6 +154,15 @@ lvim.plugins = {
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     opts = {},
   },
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+    end,
+  },
 }
 
 ------------------------

cf0641841442bd68a2be3290bb24dd6581c1c4d9
Zachary Taylor <zacharytaylor@tutamail.com>
2023-11-13 13:41:14 -0600

    sbar config tweaks for update

--- config/sketchybar/items/front_app.sh
+++ config/sketchybar/items/front_app.sh
@@ -5,10 +5,9 @@
 front_app=(
   label.font="$FONT:Black:12.0"
   icon.background.drawing=on
-  # icon.drawing=off
-  associated_display=active
-  # script="$FRONT_APP_SCRIPT"
+  display=active
   script="$PLUGIN_DIR/front_app.sh"
+  click_script="open -a 'Mission Control'"
 )
 
 sketchybar --add item front_app left         \

a59e4e20e4a1d73b9d44bd93d654ae518d723518
Zachary Taylor <zacharytaylor@tutamail.com>
2023-12-29 13:50:46 -0600

    I'm totally adding this to epic win. +300

--- zsh/aliases/git.zsh
+++ zsh/aliases/git.zsh
@@ -298,7 +298,7 @@ open-github-pulls() {
 alias ghp='open-github-pulls'
 
 # Lulz
-alias gitgud=git commit -m "$(curl -s https://whatthecommit.com/index.txt)"
+alias gitgud='git commit -m "$(curl -s https://whatthecommit.com/index.txt)"'
 
 # Prompt for main SSH key passphrase, so u don't need to enter it again until session killed
 alias add-key='eval "$(ssh-agent)" && ssh-add ~/.ssh/id_rsa'

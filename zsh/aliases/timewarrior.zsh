cc15d6c6e8cbd85b1690c13773b935192689164a
Zachary Taylor <zacharytaylor@tutamail.com>
2023-12-29 13:50:36 -0600

    adding timewarrior

---
+++ zsh/aliases/timewarrior.zsh
@@ -0,0 +1,29 @@
+alias tw=timew
+
+# Summary
+alias tws='timew summary'
+alias twsi='timew summary :ids'
+alias twsia='timew summary :ids :annotations'
+alias twsy='timew summary :yesterday'
+alias twsw='timew summary :week'
+alias twds='timew day summary'
+alias twws='timew week summary'
+alias twms='timew month summary'
+
+# Tracking
+alias twa='timew start'
+alias two='timew stop'
+alias twc='timew continue'
+alias twt='timew track'
+alias twl='timew lengthen'
+alias twsh='timew shorten'
+alias twm='timew modify'
+alias twma='timew modify start'
+alias twmo='timew modify end'
+alias twrs='timew resize'
+alias twz='timew undo'
+alias twd='timew delete'
+
+# Labeling
+alias twann='timew annotate'
+alias twtag='timew tag'

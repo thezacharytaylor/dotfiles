0f537f6092f10bf7f4c43245cdb037349cac2cff
Zachary Taylor <zacharytaylor@tutamail.com>
2024-01-22 16:57:38 -0600

    cleaning up wordpress script

--- zsh/aliases/wordpress.zsh
+++ zsh/aliases/wordpress.zsh
@@ -9,6 +9,10 @@ setupwp() {
   git clone --depth=1 git@github.com:KineticTeam/$1-$2-$3.git $2 && cd $2
   wp core download
   
+  if [[ -f DELETEME.zip ]]; then
+    unzip DELETEME.zip
+  fi
+  
   if [[ ! -f wp-config.php ]]; then
     cp wp-config-sample.php wp-config.php
   fi
@@ -27,9 +31,13 @@ finishwp() {
     dbname=$3
   fi
 
-  wp db create
-  wp db import $dbname.sql
-  wp search-replace "$1" "$2" --all-tables
-  wp cache flush
-  v
+  if [[ -f $dbname.sql ]]; then
+    wp db create
+    wp db import $dbname.sql
+    wp search-replace "$1" "$2" --all-tables
+    wp cache flush
+    v
+  else
+    echo "No database file found."
+  fi 
 }

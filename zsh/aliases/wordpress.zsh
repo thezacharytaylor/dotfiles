17ea1b73326f1ba44b0d3ddaf8e9609b298b403c
Zachary Taylor <zacharytaylor@tutamail.com>
2024-01-22 16:28:53 -0600

    adding new wp setup script for future working.

---
+++ zsh/aliases/wordpress.zsh
@@ -0,0 +1,16 @@
+# setupwp
+
+setupwp() {
+  # clone repo
+  # get wp core
+  # cp wp-config-sample.php wp-config.php
+  # v
+}
+
+finishwp() {
+  # create db
+  # import db
+  # searsch replace
+  # cache flush
+  # v
+}

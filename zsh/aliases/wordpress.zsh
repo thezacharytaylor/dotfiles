2817bc5364c29c981e3593e1c81f5281ac4385db
Zachary Taylor <zacharytaylor@tutamail.com>
2024-01-22 16:50:07 -0600

    writing out quick wp setup for old existing sites

--- zsh/aliases/wordpress.zsh
+++ zsh/aliases/wordpress.zsh
@@ -2,15 +2,34 @@
 
 setupwp() {
   # clone repo
-  # get wp core
-  # cp wp-config-sample.php wp-config.php
-  # v
+  if [[ "$#" -lt 3 ]]; then
+    echo "Invalid number of arguments, expected 3. Client code, domain name, domain extension."
+  fi
+
+  git clone --depth=1 git@github.com:KineticTeam/$1-$2-$3.git $2 && cd $2
+  wp core download
+  
+  if [[ ! -f wp-config.php ]]; then
+    cp wp-config-sample.php wp-config.php
+  fi
+
+  v
 }
 
 finishwp() {
-  # create db
-  # import db
-  # searsch replace
-  # cache flush
-  # v
+  local dbname="DELETEME"
+
+  if [[ "$#" -lt 2 ]]; then
+    echo "Invalid number of arguments, expected 2. Old domain name and test domain name."
+  fi
+
+  if [[ -n "$3" ]]; then
+    dbname=$3
+  fi
+
+  wp db create
+  wp db import $dbname.sql
+  wp search-replace "$1" "$2" --all-tables
+  wp cache flush
+  v
 }

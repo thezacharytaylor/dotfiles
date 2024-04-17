09583f790f441aeb4a40993d643d40718f5f7ead
Zachary Taylor <zacharytaylor@tutamail.com>
2024-04-17 09:54:54 -0500

    adding more possible themes for future use

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -2,7 +2,7 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
--- Other themes: 'ray-x/starry.nvim'
+-- Other themes: "ray-x/starry.nvim", "sts10/vim-pink-moon", "oxfist/night-owl.nvim"
 
 ------------------------
 -- Plugins

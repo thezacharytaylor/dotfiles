cbe79e375dab69c75b751aca2bbd0d8d0e51dd5e
Zachary Taylor <zacharytaylor@tutamail.com>
2024-05-30 15:33:15 -0500

    adding some options to astro setup, scroll off

--- config/nvim/lua/polish.lua
+++ config/nvim/lua/polish.lua
@@ -1,6 +1,19 @@
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+--
+-- Set some options
+vim.opt.spell = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wrap = false
+vim.opt.breakindent = true -- maintain indent when wrapping indented lines
+vim.opt.linebreak = true -- wrap at word boundaries
+vim.opt.list = true -- enable the below listchars
+vim.opt.listchars = { tab = "▸ ", trail = "·", nbsp = "␣" }
+vim.opt.fillchars:append { eob = " " } -- remove the ~ from end of buffer
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
 
 -- Set up custom filetypes
 vim.filetype.add {

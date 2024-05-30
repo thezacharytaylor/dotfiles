335a5e8fd8d12d87470fc5e338f1ea4a5b164995
Zachary Taylor <zacharytaylor@tutamail.com>
2024-05-30 15:27:47 -0500

    tweaks to volume in sketchybar

--- config/sketchybar/items/widgets/volume.lua
+++ config/sketchybar/items/widgets/volume.lua
@@ -9,8 +9,8 @@ local volume_percent = sbar.add("item", "widgets.volume1", {
   icon = { drawing = false },
   label = {
     string = "??%",
-    padding_left = -1,
-    font = { family = settings.font.numbers }
+    padding_left = 0,
+    font = { family = settings.font.numbers },
   },
 })
 
@@ -39,15 +39,15 @@ local volume_icon = sbar.add("item", "widgets.volume2", {
 
 local volume_bracket = sbar.add("bracket", "widgets.volume.bracket", {
   volume_icon.name,
-  volume_percent.name
+  volume_percent.name,
 }, {
   background = { color = colors.bg1 },
-  popup = { align = "center" }
+  popup = { align = "center" },
 })
 
 sbar.add("item", "widgets.volume.padding", {
   position = "right",
-  width = settings.group_paddings
+  width = settings.group_paddings,
 })
 
 local volume_slider = sbar.add("slider", popup_width, {
@@ -59,13 +59,13 @@ local volume_slider = sbar.add("slider", popup_width, {
       corner_radius = 3,
       color = colors.bg2,
     },
-    knob= {
+    knob = {
       string = "􀀁",
       drawing = true,
     },
   },
   background = { color = colors.bg1, height = 2, y_offset = -20 },
-  click_script = 'osascript -e "set volume output volume $PERCENTAGE"'
+  click_script = 'osascript -e "set volume output volume $PERCENTAGE"',
 })
 
 volume_percent:subscribe("volume_change", function(env)
@@ -93,9 +93,11 @@ end)
 
 local function volume_collapse_details()
   local drawing = volume_bracket:query().popup.drawing == "on"
-  if not drawing then return end
+  if not drawing then
+    return
+  end
   volume_bracket:set({ popup = { drawing = false } })
-  sbar.remove('/volume.device\\.*/')
+  sbar.remove("/volume.device\\.*/")
 end
 
 local current_audio_device = "None"
@@ -115,7 +117,7 @@ local function volume_toggle_details(env)
         local color = colors.grey
         local counter = 0
 
-        for device in string.gmatch(available, '[^\r\n]+') do
+        for device in string.gmatch(available, "[^\r\n]+") do
           local color = colors.grey
           if current == device then
             color = colors.white
@@ -125,8 +127,12 @@ local function volume_toggle_details(env)
             width = popup_width,
             align = "center",
             label = { string = device, color = color },
-            click_script = 'SwitchAudioSource -s "' .. device .. '" && sketchybar --set /volume.device\\.*/ label.color=' .. colors.grey .. ' --set $NAME label.color=' .. colors.white
-
+            click_script = 'SwitchAudioSource -s "'
+              .. device
+              .. '" && sketchybar --set /volume.device\\.*/ label.color='
+              .. colors.grey
+              .. " --set $NAME label.color="
+              .. colors.white,
           })
           counter = counter + 1
         end
@@ -147,4 +153,3 @@ volume_icon:subscribe("mouse.scrolled", volume_scroll)
 volume_percent:subscribe("mouse.clicked", volume_toggle_details)
 volume_percent:subscribe("mouse.exited.global", volume_collapse_details)
 volume_percent:subscribe("mouse.scrolled", volume_scroll)
-

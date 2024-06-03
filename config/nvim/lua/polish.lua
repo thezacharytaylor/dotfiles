cd73d4ac208cae93c1c54f6d34c41a5eb9937ee5
Zachary Taylor <zacharytaylor@tutamail.com>
2024-06-03 10:01:27 -0500

    tweaks for options

--- config/nvim/lua/polish.lua
+++ config/nvim/lua/polish.lua
@@ -3,6 +3,12 @@
 -- fit in the normal config locations above can go here
 --
 -- Set some options
+vim.opt.cmdheight = 0
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 vim.opt.spell = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
@@ -14,6 +20,10 @@ vim.opt.listchars = { tab = "▸ ", trail = "·", nbsp = "␣" }
 vim.opt.fillchars:append { eob = " " } -- remove the ~ from end of buffer
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.completeopt = "menuone,longest,preview"
+vim.opt.showmode = false
 
 -- Set up custom filetypes
 vim.filetype.add {

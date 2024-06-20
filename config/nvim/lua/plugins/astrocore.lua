94891fb52c0d689972ecf03ef455907a8d581a65
Zachary Taylor <zacharytaylor@tutamail.com>
2024-06-20 10:10:47 -0500

    astrocore tweaks to add wrapping and H + L for buffer nav

--- config/nvim/lua/plugins/astrocore.lua
+++ config/nvim/lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -29,9 +27,9 @@ return {
       opt = { -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-        wrap = false, -- sets vim.opt.wrap
+        spell = true, -- sets vim.opt.spell
+        signcolumn = "yes:2", -- sets vim.opt.signcolumn to auto
+        wrap = true, -- sets vim.opt.wrap
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)

c778adf11304027dd1cc73f47b5cc61b99dd5f48
Zachary Taylor <zacharytaylor@tutamail.com>
2024-09-24 09:44:41 -0500

    astrolsp tweak

--- config/nvim/lua/plugins/astrolsp.lua
+++ config/nvim/lua/plugins/astrolsp.lua
@@ -24,7 +24,7 @@ return {
           -- "go",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
+          "php",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers

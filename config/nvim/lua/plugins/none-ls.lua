4fd9b4721ec64338dca565c1845b8e5bd7359f43
Zachary Taylor <zacharytaylor@tutamail.com>
2024-10-31 11:35:46 -0500

    none-ls formatting, for blade

--- config/nvim/lua/plugins/none-ls.lua
+++ config/nvim/lua/plugins/none-ls.lua
@@ -1,19 +1,47 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize None-ls sources
 
 ---@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
+  main = "null-ls",
   opts = function(_, opts)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
       -- Set a formatter
+      null_ls.builtins.formatting.prettierd.with {
+        filetypes = {
+          "javascript",
+          "typescript",
+          "javascriptreact",
+          "typescriptreact",
+          "css",
+          "postcss",
+          "scss",
+          "json",
+          "yaml",
+          "html",
+          "markdown",
+          "vue",
+        },
+      },
+      null_ls.builtins.formatting.phpcsfixer.with {
+        filetypes = { "php" },
+        disabled_filetypes = { "blade" },
+      },
+      null_ls.builtins.formatting.blade_formatter.with {
+        filetypes = { "blade" },
+        extra_args = {
+          "--indentSize=2",
+          "--noMultipleEmptyLines=true",
+          "--sortHtmlAttributes=code-guide",
+          "--wrapAttributes=force-expand-multiline",
+        },
+      },
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
     })

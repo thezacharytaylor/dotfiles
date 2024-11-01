752fd6007d0a773ce0882eb350ed175a66b9dc60
Zachary Taylor <zacharytaylor@tutamail.com>
2024-11-01 09:59:14 -0500

    more sail aliases

--- zsh/aliases/laravel.zsh
+++ zsh/aliases/laravel.zsh
@@ -1,3 +1,12 @@
+# Sail aliases
 alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
+
+# Sail control
 alias sud='sail up -d'
 alias sdd='sail down'
+
+# Sail commands
+alias sart='sail artisan'
+alias scom='sail composer'
+alias sphp='sail php'
+alias sam='sail artisan migrate'
